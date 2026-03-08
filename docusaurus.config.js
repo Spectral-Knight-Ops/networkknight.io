8c29d1d73a7349966a2a6bd00dea57b1841df5d2
Jordan Wylie <jordan@mac.mynetworksettings.com>
2026-03-08 17:04:03 -0400

    fix favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 const config = {
   title: 'NetworkKnight',
   tagline: 'Cybersecurity Knowledge Base',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/knight.png',
 
   // GitHub Pages deployment config
   url: 'https://networkknight.io',
